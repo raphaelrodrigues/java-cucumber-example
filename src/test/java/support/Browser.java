85150e0ccfecd67c28f509e3fc6fac3daa35cbcf
raphael <raphael.rodrigues@vilt-group.com>
2019-04-12 16:10:33 +0100

    Test

--- src/test/java/support/Browser.java
+++ src/test/java/support/Browser.java
@@ -4,19 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Browser {
 	
 	public static WebDriver launch() {
 		String browser = System.getProperty("browser");
-		if(browser.equals("firefox")) {
-			return new FirefoxDriver();
-		} else if(browser.equals("chrome")) {
-			return new ChromeDriver();
-		} else if(browser.equals("msie")) {
-			return new InternetExplorerDriver();
-		} else {
-			throw new RuntimeException("Unrecognized system property 'browser': " + browser);
-		}
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--headless");
+		chromeOptions.addArguments("--window-size=1200x600");
+		return new ChromeDriver(chromeOptions);
 	}
 }
